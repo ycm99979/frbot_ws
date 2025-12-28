8179416e6dcd3fccc820b3a3fcd1d0cbb6127bf1
ycm99979 <ycm99979@gmail.com>
2025-12-29 00:23:15 +0900

    Delete drivers/md_motor_hardware/myactuator_rmd directory

--- drivers/md_motor_hardware/myactuator_rmd/myactuator_rmd/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@file __init__.py
-@mainpage
-   Import Python bindings from myactuator_rmd_py
-@author
-   Tobit Flatscher (github.com/2b-t)
-"""
-
-from myactuator_rmd import myactuator_rmd_py
